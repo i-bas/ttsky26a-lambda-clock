1212489e6a22a2623c8901804ce920877b2c4dc6
ibrahimbas <153763446+i-bas@users.noreply.github.com>
2026-03-19 01:16:46 +0300

    Update and rename tt_um_i-bas_clock_alarm.v to tt_um_ibas_clock_alarm.v

--- src/tt_um_ibas_clock_alarm.v
+++ src/tt_um_ibas_clock_alarm.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_i-bas_clock_alarm (
+module tt_um_ibas_clock_alarm (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // Bidirectional inputs
